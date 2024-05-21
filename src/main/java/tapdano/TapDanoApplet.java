9b0ba5fc7ba667cc4e1badecde455fb7a67137d3
Lucas Boemeke <lucas@blueshift.cc>
2024-05-21 02:04:30 -0300

    pin, two-factor & lastsignature

--- src/main/java/tapdano/TapDanoApplet.java
+++ src/main/java/tapdano/TapDanoApplet.java
@@ -9,16 +9,19 @@ public class TapDanoApplet extends Applet implements TapDanoShareable {
   boolean INITIALIZED = false;
   boolean PAIR_GENERATED = false;
   boolean SIGN_INITIALIZED = false;
-  
+
   byte[] priKeyEncoded = new byte[32];
   byte[] pubKeyEncoded = new byte[32];
   byte TAG_TYPE;
   boolean TAG_EXTRACT_LOCKED;
+  boolean PIN_LOCKED;
+  byte[] PIN = new byte[4];
 
   byte[] lastBuffer = new byte[256];
   byte[] lastResponse = new byte[256];
-  byte[] NDEF_LastResponse = new byte[256];
-  short NDEF_LastLength;
+  byte[] LAST_SIGNATURE = new byte[64];
+  byte[] TWO_FACTOR_KEY = new byte[32];
+
   short lastResponseLen;
 
   NamedParameterSpec params;
@@ -26,6 +29,7 @@ public class TapDanoApplet extends Applet implements TapDanoShareable {
   XECKey pubkey;
   KeyPair keypair;
   Signature signature;
+  MessageDigest sha256;
 
   protected TapDanoApplet(byte[] buf, short offAID, byte lenAID) {
     if (Constants.DEBUG) System.out.println("TapDanoApplet constructor");
@@ -54,112 +58,103 @@ public class TapDanoApplet extends Applet implements TapDanoShareable {
     if (selectingApplet()) return;
     byte[] buffer = apdu.getBuffer();
     try {
-      short outputLength = processTapDano(buffer, (byte)0, (byte)0);
-      apdu.setOutgoingAndSend((short)0, outputLength);
+      short outputLength = processTapDano(buffer, (byte) 0, (byte) 0);
+      apdu.setOutgoingAndSend((short) 0, outputLength);
     } catch (Exception e) {
-      ISOException.throwIt((short)0xFF01);
+      ISOException.throwIt((short) 0xFF01);
     }
   }
 
   public short exec(byte origin, byte[] buffer, byte offsetIn) {
-    boolean isNDEF_Read = (origin == (byte)0x03);
-    boolean isNDEF_Write = (origin == (byte)0x01);
-    boolean isFIDO = (origin == (byte)0x02);
+    boolean isNDEF_Read = (origin == (byte) 0x03);
+    boolean isNDEF_Write = (origin == (byte) 0x01);
+    boolean isFIDO = (origin == (byte) 0x02);
 
-    byte offsetOut = (byte)5;
-    short outputLength = (short)0;
-
-    if (isNDEF_Read) {
-      Util.arrayCopy(NDEF_LastResponse, (short)0, buffer, (short)0, NDEF_LastLength);
-      return NDEF_LastLength;
-    }
+    byte offsetOut = (byte) 5;
+    short outputLength = (short) 0;
 
     try {
       if (!INITIALIZED) initialize();
       outputLength = processTapDano(buffer, offsetIn, offsetOut);
     } catch (Exception e) {
       if (Constants.DEBUG) System.out.println("############### Exception ###############");
-      outputLength = (short)1;
-      buffer[offsetOut] = (byte)0x50;
+      outputLength = (short) 1;
+      buffer[offsetOut] = (byte) 0x50;
     }
 
-    if (isNDEF_Write) {
-      buffer[0] = (byte)0x00;
-      buffer[1] = (byte)(outputLength + 3);
-      buffer[2] = (byte)0xD5;
-      buffer[3] = (byte)0x00;
-      buffer[4] = (byte)outputLength;
-      outputLength += (short)5;
-      Util.arrayCopy(buffer, (short)0, NDEF_LastResponse, (short)0, outputLength);
-      NDEF_LastLength = outputLength;
+    if (isNDEF_Read || isNDEF_Write) {
+      buffer[0] = (byte) 0x00;
+      buffer[1] = (byte) (outputLength + 3);
+      buffer[2] = (byte) 0xD5;
+      buffer[3] = (byte) 0x00;
+      buffer[4] = (byte) outputLength;
+      outputLength += (short) 5;
     }
 
     if (isFIDO) {
-      outputLength += (byte)5;
-      buffer[0] = (byte)0x01;
-      buffer[1] = (byte)0x00;
-      buffer[2] = (byte)0x00;
-      buffer[3] = (byte)0x00;
-      buffer[4] = (byte)0x01;
+      outputLength += (byte) 5;
+      buffer[0] = (byte) 0x01;
+      buffer[1] = (byte) 0x00;
+      buffer[2] = (byte) 0x00;
+      buffer[3] = (byte) 0x00;
+      buffer[4] = (byte) 0x01;
     }
 
     return outputLength;
   }
 
   public short processTapDano(byte[] buffer, byte offsetIn, byte offsetOut) {
-    short dataLen = (short)buffer[(byte)(offsetIn + ISO7816.OFFSET_LC)];
+    short dataLen = (short) buffer[(byte) (offsetIn + ISO7816.OFFSET_LC)];
     byte offsetOutOriginal = offsetOut;
 
-    boolean isCacheActive = (buffer[(byte)(offsetIn + ISO7816.OFFSET_P1)] == (byte)0x01);
+    boolean isCacheActive = (buffer[(byte) (offsetIn + ISO7816.OFFSET_P1)] == (byte) 0x01);
     byte[] lastBufferTemp = new byte[256];
 
     if (isCacheActive) {
-      if (Util.arrayCompare(buffer, (short)offsetIn, lastBuffer, (short)0, (short)(256 - offsetIn)) == (byte)0) {
-        Util.arrayCopy(lastResponse, (short)0, buffer, (short)offsetOut, (short)(256 - offsetOut));
+      if (Util.arrayCompare(buffer, (short) offsetIn, lastBuffer, (short) 0, (short) (256 - offsetIn)) == (byte) 0) {
+        Util.arrayCopy(lastResponse, (short) 0, buffer, (short) offsetOut, (short) (256 - offsetOut));
         return lastResponseLen;
       }
-      dataLen = (short)(dataLen - 8);
-      Util.arrayCopy(buffer, (short)offsetIn, lastBufferTemp, (short)0, (short)(256 - offsetIn));
-    }
-
-    short responseLen = (short)0;
-
-    // Get Info
-    if (buffer[(byte)(offsetIn + ISO7816.OFFSET_INS)] == (byte)0xA0) {
-      responseLen = getTagInfo(buffer, offsetIn, offsetOut, dataLen);
+      dataLen = (short) (dataLen - 8);
+      Util.arrayCopy(buffer, (short) offsetIn, lastBufferTemp, (short) 0, (short) (256 - offsetIn));
     }
 
     // Burn Tag
-    if (buffer[(byte)(offsetIn + ISO7816.OFFSET_INS)] == (byte)0xA1) {
-      responseLen = burnTag(buffer, offsetIn, offsetOut, dataLen);
+    if (buffer[(byte) (offsetIn + ISO7816.OFFSET_INS)] == (byte) 0xA1) {
+      burnTag(buffer, offsetIn, offsetOut, dataLen);
     }
 
     // Sign Data
-    if (buffer[(byte)(offsetIn + ISO7816.OFFSET_INS)] == (byte)0xA2) {
-      responseLen = signData(buffer, offsetIn, offsetOut, dataLen);
+    if (buffer[(byte) (offsetIn + ISO7816.OFFSET_INS)] == (byte) 0xA2) {
+      signData(buffer, offsetIn, offsetOut, dataLen);
     }
 
     // Format Tag
-    if (buffer[(byte)(offsetIn + ISO7816.OFFSET_INS)] == (byte)0xA3) {
-      responseLen = formatTag(buffer, offsetIn, offsetOut, dataLen);
+    if (buffer[(byte) (offsetIn + ISO7816.OFFSET_INS)] == (byte) 0xA3) {
+      formatTag(buffer, offsetIn, offsetOut, dataLen);
     }
 
     // Lock Tag
-    if (buffer[(byte)(offsetIn + ISO7816.OFFSET_INS)] == (byte)0xA4) {
-      responseLen = lockTag(buffer, offsetIn, offsetOut, dataLen);
+    if (buffer[(byte) (offsetIn + ISO7816.OFFSET_INS)] == (byte) 0xA4) {
+      lockTag(buffer, offsetIn, offsetOut, dataLen);
     }
 
-    //INS not found
-    if (responseLen == (short)0) {
-      buffer[offsetOut++] = (byte)0x6D;
-      buffer[offsetOut++] = (byte)0x00;
-      responseLen = (short)2;
+    // Pin Lock
+    if (buffer[(byte) (offsetIn + ISO7816.OFFSET_INS)] == (byte) 0xA5) {
+      pinLock(buffer, offsetIn, offsetOut, dataLen);
     }
+
+    // Pin Unlock
+    if (buffer[(byte) (offsetIn + ISO7816.OFFSET_INS)] == (byte) 0xA6) {
+      pinUnlock(buffer, offsetIn, offsetOut, dataLen);
+    }
+
+    short responseLen = getTagInfo(buffer, offsetIn, offsetOut, dataLen);
 
     if (isCacheActive) {
       lastResponseLen = responseLen;
-      Util.arrayCopy(lastBufferTemp, (short)0, lastBuffer, (short)0, (short)lastBuffer.length);
-      Util.arrayCopy(buffer, (short)offsetOutOriginal, lastResponse, (short)0, (short)(256 - offsetOutOriginal));
+      Util.arrayCopy(lastBufferTemp, (short) 0, lastBuffer, (short) 0, (short) lastBuffer.length);
+      Util.arrayCopy(buffer, (short) offsetOutOriginal, lastResponse, (short) 0, (short) (256 - offsetOutOriginal));
     }
 
     return responseLen;
@@ -176,95 +171,119 @@ public class TapDanoApplet extends Applet implements TapDanoShareable {
     attributes |= JCSystem.MEMORY_TYPE_PERSISTENT;
     pubkey = (XECKey) KeyBuilder.buildXECKey(params, attributes, false);
 
-    keypair = new KeyPair((PublicKey)pubkey,(PrivateKey)prikey);
+    keypair = new KeyPair((PublicKey) pubkey, (PrivateKey) prikey);
 
     signature = Signature.getInstance(MessageDigest.ALG_NULL, Signature.SIG_CIPHER_EDDSA, Cipher.PAD_NULL, false);
+
+    sha256 = MessageDigest.getInstance(MessageDigest.ALG_SHA_256, false);
 
     INITIALIZED = true;
     SIGN_INITIALIZED = false;
     PAIR_GENERATED = false;
     priKeyEncoded = new byte[32];
     pubKeyEncoded = new byte[32];
+    TWO_FACTOR_KEY = new byte[32];
+    LAST_SIGNATURE = new byte[64];
   }
 
   private short getTagInfo(byte[] buffer, byte offsetIn, byte offsetOut, short dataLen) {
-    short responseLen = (short)0;
-    buffer[offsetOut++] = (byte)0x54; // T[ap]
-    buffer[offsetOut++] = (byte)0x44; // D[ano]
-    buffer[offsetOut++] = (byte)0x01; // Version
-    buffer[offsetOut++] = (byte)0x00; // Version
-    buffer[offsetOut++] = PAIR_GENERATED ? (byte)0x01 : (byte)0x00;
-    responseLen = (short)0x00005;
+    short responseLen = (short) 0;
+    buffer[offsetOut++] = (byte) 0x54; // T[ap]
+    buffer[offsetOut++] = (byte) 0x44; // D[ano]
+    buffer[offsetOut++] = (byte) 0x01; // Version
+    buffer[offsetOut++] = (byte) 0x00; // Version
+    buffer[offsetOut++] = PAIR_GENERATED ? (byte) 0x01 : (byte) 0x00;
+    responseLen = (short) 0x00005;
     if (PAIR_GENERATED) {
-      boolean showPk = (TAG_TYPE == (byte)0x02) && (!TAG_EXTRACT_LOCKED);
       buffer[offsetOut++] = TAG_TYPE;
-      buffer[offsetOut++] = TAG_EXTRACT_LOCKED ? (byte)0x01 : (byte)0x00;
-      Util.arrayCopyNonAtomic(pubKeyEncoded, (short)0, buffer, (short)offsetOut, (short)pubKeyEncoded.length);
-      offsetOut += (short)pubKeyEncoded.length;
-      responseLen += (short)pubKeyEncoded.length + 2;
-      if (showPk) {
-        Util.arrayCopyNonAtomic(priKeyEncoded, (short)0, buffer, (short)offsetOut, (short)priKeyEncoded.length);
-        offsetOut += (short)priKeyEncoded.length;
-        responseLen += (short)priKeyEncoded.length;
+      buffer[offsetOut++] = TAG_EXTRACT_LOCKED ? (byte) 0x01 : (byte) 0x00;
+      buffer[offsetOut++] = PIN_LOCKED ? (byte) 0x01 : (byte) 0x00;
+      responseLen += 3;
+
+      boolean showPk = (TAG_TYPE == (byte) 0x02) && (!TAG_EXTRACT_LOCKED) && (!PIN_LOCKED);
+      Util.arrayCopyNonAtomic(showPk ? priKeyEncoded : pubKeyEncoded, (short) 0, buffer, (short) offsetOut, (short) 32);
+      offsetOut += (short) 32;
+      responseLen += (short) 32;
+
+      if (!PIN_LOCKED) {
+        Util.arrayCopyNonAtomic(TWO_FACTOR_KEY, (short) 0, buffer, (short) offsetOut, (short) TWO_FACTOR_KEY.length);
+        offsetOut += (short) TWO_FACTOR_KEY.length;
+        responseLen += (short) TWO_FACTOR_KEY.length;
+
+        Util.arrayCopyNonAtomic(LAST_SIGNATURE, (short) 0, buffer, (short) offsetOut, (short) LAST_SIGNATURE.length);
+        offsetOut += (short) LAST_SIGNATURE.length;
+        responseLen += (short) LAST_SIGNATURE.length;
       }
     }
     return responseLen;
   }
 
-  private short burnTag(byte[] buffer, byte offsetIn, byte offsetOut, short dataLen) {
-    byte action = buffer[(short)(offsetIn + ISO7816.OFFSET_CDATA)];
-    byte type = buffer[(short)(offsetIn + ISO7816.OFFSET_CDATA + 1)];
-    byte[] importPrivateKey = new byte[32];
+  private void burnTag(byte[] buffer, byte offsetIn, byte offsetOut, short dataLen) {
+    byte action = buffer[(short) (offsetIn + ISO7816.OFFSET_CDATA)];
+    byte type = buffer[(short) (offsetIn + ISO7816.OFFSET_CDATA + 1)];
 
-    if (PAIR_GENERATED) {
-      return getTagInfo(buffer, offsetIn, offsetOut, dataLen);
-    }
-    
-    if (action == (byte)0x02) { //restore
-      if (type == (byte)0x01) { //soulbound
-        buffer[offsetOut++] = (byte)0xEE;
-        buffer[offsetOut++] = (byte)0x02;
-        return (short)2;
+    if (PAIR_GENERATED) return;
+
+    if (action == (byte) 0x02) { // restore
+      if (type == (byte) 0x01) { // soulbound
+        return;
       }
-      Util.arrayCopyNonAtomic(buffer, (short)(offsetIn + ISO7816.OFFSET_CDATA + 2), importPrivateKey, (short)0, (short)importPrivateKey.length);
     }
 
     TAG_TYPE = type;
-    TAG_EXTRACT_LOCKED = (type == (byte)0x01);
+    TAG_EXTRACT_LOCKED = (type == (byte) 0x01);
+    PIN_LOCKED = false;
 
-    if (action == (byte)0x01) { //new
+    if (action == (byte) 0x01) { // new
       keypair.genKeyPair();
-      prikey = (XECKey)keypair.getPrivate();
-      pubkey = (XECKey)keypair.getPublic();
-      prikey.getEncoded(priKeyEncoded, (short)0);
-      pubkey.getEncoded(pubKeyEncoded, (short)0);
+      prikey = (XECKey) keypair.getPrivate();
+      pubkey = (XECKey) keypair.getPublic();
+      prikey.getEncoded(priKeyEncoded, (short) 0);
+      pubkey.getEncoded(pubKeyEncoded, (short) 0);
     }
 
-    if (action == (byte)0x02) { //restore
-      // TO DO
+    if (action == (byte) 0x02) { // restore
+      prikey.setEncoded(buffer, (short) (offsetIn + ISO7816.OFFSET_CDATA + 2), (short) priKeyEncoded.length);
+      pubkey.setEncoded(buffer, (short) (offsetIn + ISO7816.OFFSET_CDATA + 34), (short) pubKeyEncoded.length);
+      Util.arrayCopyNonAtomic(buffer, (short) (offsetIn + ISO7816.OFFSET_CDATA + 2), priKeyEncoded, (short) 0, (short) priKeyEncoded.length);
+      Util.arrayCopyNonAtomic(buffer, (short) (offsetIn + ISO7816.OFFSET_CDATA + 34), pubKeyEncoded, (short) 0, (short) pubKeyEncoded.length);
     }
+
+    // TWO_FACTOR_KEY
+    byte[] dataToHash = new byte[33];
+    Util.arrayCopyNonAtomic(priKeyEncoded, (short) 0, dataToHash, (short) 0, (short) 32);
+    dataToHash[32] = (byte) 0x01;
+    sha256.doFinal(dataToHash, (short) 0, (short) dataToHash.length, TWO_FACTOR_KEY, (short) 0);
 
     PAIR_GENERATED = true;
-
-    return getTagInfo(buffer, offsetIn, offsetOut, dataLen);
   }
 
-  public short signData(byte[] buffer, byte offsetIn, byte offsetOut, short dataLen) throws CryptoException {
+  public void signData(byte[] buffer, byte offsetIn, byte offsetOut, short dataLen) throws CryptoException {
     if (!SIGN_INITIALIZED) {
-      signature.init((Key)prikey, Signature.MODE_SIGN);
+      signature.init((Key) prikey, Signature.MODE_SIGN);
       SIGN_INITIALIZED = true;
     }
-    signature.sign(buffer, (short)(offsetIn + ISO7816.OFFSET_CDATA), dataLen, buffer, (short)offsetOut);
-    return (short)64;
+    signature.sign(buffer, (short) (offsetIn + ISO7816.OFFSET_CDATA), dataLen, LAST_SIGNATURE, (short) 0);
   }
 
-  private short formatTag(byte[] buffer, byte offsetIn, byte offsetOut, short dataLen) {
+  private void formatTag(byte[] buffer, byte offsetIn, byte offsetOut, short dataLen) {
     initialize();
-    return getTagInfo(buffer, offsetIn, offsetOut, dataLen);
   }
 
-  private short lockTag(byte[] buffer, byte offsetIn, byte offsetOut, short dataLen) {
+  private void lockTag(byte[] buffer, byte offsetIn, byte offsetOut, short dataLen) {
     TAG_EXTRACT_LOCKED = true;
-    return getTagInfo(buffer, offsetIn, offsetOut, dataLen);
+  }
+
+  private void pinLock(byte[] buffer, byte offsetIn, byte offsetOut, short dataLen) {
+    Util.arrayCopyNonAtomic(buffer, (short) (offsetIn + ISO7816.OFFSET_CDATA), PIN, (short) 0, (short) PIN.length);
+    PIN_LOCKED = true;
+  }
+
+  private void pinUnlock(byte[] buffer, byte offsetIn, byte offsetOut, short dataLen) {
+    byte[] inputPin = new byte[PIN.length];
+    Util.arrayCopyNonAtomic(buffer, (short) (offsetIn + ISO7816.OFFSET_CDATA), inputPin, (short) 0, (short) inputPin.length);
+    if (Util.arrayCompare(inputPin, (short) 0, PIN, (short) 0, (short) PIN.length) == 0) {
+      PIN_LOCKED = false;
+    }
   }
 }
